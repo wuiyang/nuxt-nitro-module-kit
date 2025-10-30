0b733839d4c4d698f70723fe9b71b48bc41fc10b
wuiyang <wuiyang@live.com>
2025-10-30 09:44:43 +0800

    feat: added get update templates for nuxt

--- src/utils/common-nuxt.ts
+++ src/utils/common-nuxt.ts
@@ -3,3 +3,4 @@ import { buildCachedTryLoad } from "./try-load";
 export const getNuxtKit = buildCachedTryLoad("@nuxt/kit", () => import("@nuxt/kit"));
 export const tryUseNuxt = buildCachedTryLoad("tryUseNuxt", async () => (await getNuxtKit())!.tryUseNuxt());
 export const getCheckNuxtCompatibility = buildCachedTryLoad("checkNuxtCompatibility", async () => (await getNuxtKit())!.checkNuxtCompatibility);
+export const getUpdateTemplates = buildCachedTryLoad("updateTemplates", async () => (await getNuxtKit())!.updateTemplates);
